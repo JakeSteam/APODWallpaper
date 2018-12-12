9c3d46f80ee1c6c0fd0c33150daa805b23cc26a0
Jake Lee <github@jakelee.co.uk>
2018-12-12 20:46:01 +0000

    Messy commit, poor structure

---
+++ app/src/main/java/uk/co/jakelee/apodwallpaper/ApiHandler.kt
@@ -0,0 +1,32 @@
+package uk.co.jakelee.apodwallpaper
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+fun getResponse(url: String): ApodResponse {
+    val request = Request.Builder()
+        .url(url)
+        .build()
+    val response = OkHttpClient().newCall(request).execute()
+    if (response.isSuccessful) {
+        response.body()?.string()?.let {
+            return Gson().fromJson(it, ApodResponse::class.java)
+        }
+        throw IOException()
+    } else {
+        throw IOException()
+    }
+}
+
+data class ApodResponse(
+    val copyright: String,
+    val date: String,
+    val explanation: String,
+    val hdurl: String,
+    val media_type: String,
+    val service_version: String,
+    val title: String,
+    val url: String
+)
--- app/src/main/java/uk/co/jakelee/apodwallpaper/MainActivity.kt
+++ app/src/main/java/uk/co/jakelee/apodwallpaper/MainActivity.kt
@@ -2,13 +2,16 @@ package uk.co.jakelee.apodwallpaper
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.widget.Toast
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    var disposable: Disposable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,13 +21,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getApod(date: Date) {
-        val dateFormat = SimpleDateFormat("yyyy-MM-yy", Locale.UK)
-        val dateString = dateFormat.format(date)
+        val dateString = SimpleDateFormat("yyyy-MM-yy", Locale.UK).format(date)
         val url = "https://api.nasa.gov/planetary/apod?api_key=${BuildConfig.APOD_API_KEY}&date=$dateString&hd=true"
-        Log.d("app", url)
-        // perform network request
-        // parse results
-        // store locally
-        // update wallpapers
+        Timber.d("Pulling data...")
+        disposable = Single
+            .fromCallable {
+                getResponse(url)
+            }
+            .subscribe(
+                {
+                    if (checkValidResults(it)) {
+                        saveResults(it)
+                        updateWallpaper(it)
+                    }
+                },
+                { Timber.e(it) }
+            )
     }
+
+    private fun checkValidResults(response: ApodResponse) =
+        response.media_type == "image" && response.title.isNotEmpty() && response.hdurl.isNotEmpty()
+
+    private fun saveResults(response: ApodResponse) {
+        Toast.makeText(this, "Received title: ${response.title}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateWallpaper(response: ApodResponse) {
+        Toast.makeText(this, "Setting wallpaper: ${response.hdurl}", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
+    }
+
 }
