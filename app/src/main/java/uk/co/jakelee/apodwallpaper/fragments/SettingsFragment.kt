1c41ca627e1c14f4a3fa89c8ead4a6f972d7fb93
Jake Lee <github@jakelee.co.uk>
2018-12-15 14:24:47 +0000

    Initial settings design

--- app/src/main/java/uk/co/jakelee/apodwallpaper/fragments/SettingsFragment.kt
+++ app/src/main/java/uk/co/jakelee/apodwallpaper/fragments/SettingsFragment.kt
@@ -3,6 +3,7 @@ package uk.co.jakelee.apodwallpaper.fragments
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import uk.co.jakelee.apodwallpaper.R
@@ -11,7 +12,15 @@ class SettingsFragment: PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         (activity as AppCompatActivity).supportActionBar!!.title = "Settings"
         setHasOptionsMenu(true)
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        addPreferencesFromResource(R.xml.preferences)
+        findPreference("view_log").setOnPreferenceClickListener {
+            Toast.makeText(activity, "View log...", Toast.LENGTH_SHORT).show()
+            true
+        }
+        findPreference("view_quota").setOnPreferenceClickListener {
+            Toast.makeText(activity, "View quota...", Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
